633da20eeb12e0e4e9653e1d2aa859c0bdfb8b6b
Moyata Ebisso <mebisso12@gmail.com>
2025-12-13 19:36:01 -0600

    update

--- app/(public)/browse/page.tsx
+++ app/(public)/browse/page.tsx
@@ -312,7 +312,7 @@ export default function BrowseProvidersPage() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Header with Search */}
+      {/* Header Search */}
       <div className="bg-white shadow-sm border-b sticky top-0 z-30">
         <div className="container mx-auto px-4 py-4">
           <div className="flex flex-col md:flex-row gap-4 items-center justify-between">
